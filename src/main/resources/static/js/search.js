598545ffe278f80c11cff79cd4aef3e9d5d7ba15
Angus Goldsmith <durins_beard@hotmail.com>
2016-02-14 17:31:41 +0000

    Prepopulates search bar on results page with searched tokens.

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -62,6 +62,14 @@ function sortResults(attribute, sortFunction) {
 var sorted;
 
 $(document).ready(function () {
+    //http://stackoverflow.com/a/31412050
+    var queryDict = {};
+    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
+
+    if(queryDict["search"]) {
+        $('.tokenfield-search').val(queryDict["search"]);
+    }
+
     initSearchTokenfield.call(this);
     sorted = false;
 });
