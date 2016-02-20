34122e2a8e501dff8083ee9f0fd6f81966a776d9
Angus Goldsmith <durins_beard@hotmail.com>
2016-02-20 12:41:59 +0000

    Added a touch on startup for Nginx buildpack.

--- src/main/java/uk/co/dashery/DasheryCompleteApplication.java
+++ src/main/java/uk/co/dashery/DasheryCompleteApplication.java
@@ -1,5 +1,6 @@
 package uk.co.dashery;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -8,10 +9,15 @@ import uk.co.dashery.clothingquery.ClothingQueryConfig;
 import uk.co.dashery.frontend.FrontEndConfig;
 import uk.co.dashery.ingestor.IngestorConfig;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
 @Import({AutocompleteConfig.class, ClothingQueryConfig.class, FrontEndConfig.class, IngestorConfig.class})
 public class DasheryCompleteApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(DasheryCompleteApplication.class, args);
+
+        FileUtils.touch(new File("/tmp/app-initialized"));
     }
 }
