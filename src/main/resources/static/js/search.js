e8290ca4c6ff8daa4358017120dd3c56a114ae23
Angus J. Goldsmith <durins_beard@hotmail.com>
2016-04-17 21:19:21 +0100

    Added on enter support.

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,4 +1,20 @@
-function search() {
+$.fn.enterKey = function (fnc) {
+    return this.each(function () {
+        $(this).keypress(function (ev) {
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
+        })
+    })
+};
 
+$(document).ready(function () {
+    $('#search-input').enterKey(function () {
+        search();
+    })
+});
+
+function search() {
     window.location.href = 'results?q=' + $("#search-input").val();
 }
